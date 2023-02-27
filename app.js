b566d2d6ac33586f043459854917156f0e5a3ac4
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-02-27 10:05:24 -0500

    split code added styling

--- app.js
+++ app.js
@@ -1,52 +1,17 @@
-"use strict";
+//"use strict";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiY2hlc2hpcmVrYXRzbWlsZSIsImEiOiJjbGVoNWZ1eHgxN2JlM3NvMHc5YzBjczV0In0.zFDkGL0zUKKJKyoGFXXsCQ";
-
-//const map = new mapboxgl.Map({
-//  container: "map",
-//  style: "mapbox://styles/mapbox/streets-v11",
-// center: [-71.092761, 42.357575],
-//  zoom: 13,
-//});
-
-const busStops = [
-  [-71.093729, 42.359244],
-  [-71.094915, 42.360175],
-  [-71.0958, 42.360698],
-  [-71.099558, 42.362953],
-  [-71.103476, 42.365248],
-  [-71.106067, 42.366806],
-  [-71.108717, 42.368355],
-  [-71.110799, 42.369192],
-  [-71.113095, 42.370218],
-  [-71.115476, 42.372085],
-  [-71.117585, 42.373016],
-  [-71.118625, 42.374863],
-];
-
-var counter = 0;
-function move() {
-  setTimeout(() => {
-    if (counter >= busStops.length) return;
-    marker.setLngLat(busStops[counter]);
-    counter++;
-    move();
-  }, 1000);
-}
-
-//var map;
+var map;
 var markers = [];
 
 // load map
 function init() {
   var myOptions = {
-    zoom: 14,
+    zoom: 12,
     center: { lat: 42.35335, lng: -71.091525 },
-    //mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
   var element = document.getElementById("map");
-  map = new mapboxgl.Map(element, myOptions);
+  map = new google.maps.Map(element, myOptions);
   addMarkers();
 }
 
@@ -119,3 +84,5 @@ function getMarker(id) {
   });
   return marker;
 }
+
+window.onload = init;
